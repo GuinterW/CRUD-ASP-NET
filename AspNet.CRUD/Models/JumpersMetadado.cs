00ee6633fbf7b71c6c0981897a20d73f92c12405
Guinter <guinterweber@hotmail.com>
2016-08-29 16:57:03 -0300

    migração de projeto.

---
+++ AspNet.CRUD/Models/JumpersMetadado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNet.CRUD.Models
+{
+    [MetadataType(typeof(JumpersMetadataValidation))]
+    public partial class Jumpers
+    {
+
+    }
+
+    public class JumpersMetadataValidation
+    {
+        [Required(ErrorMessage = "Obrigatório informar o tipo")]
+        [StringLength(30, ErrorMessage = "O tipo deve possuir no máximo 30 caracteres")]
+        public string Type { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a quantidade")]
+        public int Qntd { get; set; }
+
+    }
+}
